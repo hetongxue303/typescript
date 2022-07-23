5b9b30489d46bc30a3ac66cbcb491aa24db2534d
hetongxue303 <1974597686@qq.com>
2022-07-23 15:49:06 +0800

    enum补充

--- 02.数据类型/src/枚举.ts
+++ 02.数据类型/src/枚举.ts
@@ -10,6 +10,16 @@ value = Gender.Boy   // 结果：0
 value = Gender.Girl  // 结果：1
 
 // 枚举类型本质是数值类型 只要赋数值都不会报错
-
 value = 100
 value = 200
+
+// 默认会一上一个为主 依次+1
+enum Gender1 {
+    Boy = 10,
+    Girl
+}
+
+let value1: Gender
+
+value1 = Gender.Boy   // 结果：10
+value1 = Gender.Girl  // 结果：11
